fb6c1deed8754e953016476ed56b16d986d63b7c
mt300 <matheus.tomazi5@gmail.com>
2022-12-05 20:01:50 -0300

    second commit

--- main.py
+++ main.py
@@ -4,67 +4,71 @@ import sys
 np.set_printoptions(threshold=sys.maxsize)
 
 
-def move_action(x,y):
+def move_action(y,x):
     # 0 - up / 1 - right / 2 - down / 3 - left
-    direction = random.randint(0,3)
+    direction = random.randint(0,2)
     if(direction == 0):
-        return (x,y-1)
+        # print("Same position")
+        return (y,x-1)
     elif(direction == 1):
-        return (x+1,y)
+        return (y,x+1)
     elif(direction == 2):
-        return (x,y+1)
-    elif(direction == 3):
-        return (x-1,y)
+        return (y+1,x)
     else:
-        print("error")
-        return False
+        # print("error")
+        return (y,x)
 
-def check_neighbors(x,y,matrix):
+def check_neighbors(y,x,matrix):
     if( x == 0 ):
+        # print("aqui x==0")
         if(y == 0):
-            if( matrix[y][x+1] or matrix[y+1][x] or matrix[y+1][x+1]):
+            if( matrix[y][x+1]==1 or matrix[y+1][x]==1 or matrix[y+1][x+1]==1):
                 return True 
             else:
                 return False    
-        elif( y == matrix.shape[1]):
-            if( matrix[y-1][x] or matrix[y-1][x+1]  or  matrix[y][x+1]):
+        elif( y == (matrix.shape[0] - 1)):
+            if( matrix[y-1][x]==1 or matrix[y-1][x+1]==1  or  matrix[y][x+1]==1):
                 return True 
             else:
                 return False
         else:    
-            if( matrix[y-1][x] or matrix[y-1][x+1]  or  matrix[y][x+1] or matrix[y+1][x] or matrix[y+1][x+1]):
+            if( matrix[y-1][x]==1 or matrix[y-1][x+1]==1  or  matrix[y][x+1]==1 or matrix[y+1][x]==1 or matrix[y+1][x+1]==1):
                 return True 
             else:
                 return False
 
-    elif( x == (matrix.shape[0] - 1)):
+    elif( x == (matrix.shape[1] - 1)):
+        # print("aqui x==MAX")
         if( y == 0):
-            if(matrix[y][x-1] or matrix[y+1][x-1] or matrix[y+1][x]):
+            if(matrix[y][x-1]==1 or matrix[y+1][x-1]==1 or matrix[y+1][x]==1):
                 return True 
             else:
                 return False    
-        elif( y == matrix.shape[1]):
-            if(matrix[y-1][x-1] or matrix[y-1][x]  or matrix[y][x-1]):
+        elif( y == (matrix.shape[0] - 1)):
+            if(matrix[y-1][x-1]==1 or matrix[y-1][x]==1  or matrix[y][x-1]==1):
                 return True 
             else:
                 return False 
         else:   
-            if(matrix[y-1][x-1] or matrix[y-1][x]  or matrix[y][x-1] or matrix[y+1][x-1] or matrix[y+1][x]):
+            if(matrix[y-1][x-1]==1 or matrix[y-1][x]==1  or matrix[y][x-1]==1 or matrix[y+1][x-1]==1 or matrix[y+1][x]==1):
                 return True 
             else:
                 return False    
     elif( y == 0):
-        if( matrix[y][x-1] or  matrix[y][x+1] or matrix[y+1][x-1] or matrix[y+1][x] or matrix[y+1][x+1]):
+        # print("aqui y==0")
+        if( matrix[y][x-1]==1 or  matrix[y][x+1]==1 or matrix[y+1][x-1]==1 or matrix[y+1][x]==1 or matrix[y+1][x+1]==1):
             return True 
         else:
             return False
-    elif( y == (matrix.shape[1] - 1)):
-        if(matrix[y-1][x-1] or matrix[y-1][x] or matrix[y-1][x+1] or matrix[y][x-1] or  matrix[y][x+1]):
+    elif( y == (matrix.shape[0] - 1)):
+        # print("aqui y==MAX")
+        if(matrix[y-1][x-1]==1 or matrix[y-1][x]==1 or matrix[y-1][x+1]==1 or matrix[y][x-1]==1 or  matrix[y][x+1]==1):
             return True 
         else:
             return False 
     else:
-        if(matrix[y-1][x-1] or matrix[y-1][x] or matrix[y-1][x+1] or matrix[y][x-1] or  matrix[y][x+1] or matrix[y+1][x-1] or matrix[y+1][x] or matrix[y+1][x+1]):
+        if(matrix[y-1][x-1]==1 or matrix[y-1][x]==1 or matrix[y-1][x+1]==1 or matrix[y][x-1]==1 or  matrix[y][x+1]==1 or matrix[y+1][x-1]==1 or matrix[y+1][x]==1 or matrix[y+1][x+1]==1):
+            # print("None")
             return True 
         else:
             return False    
@@ -84,31 +88,36 @@ def ald(hig,wid,concurrent_drops,max_drops):
         start_x = random.randint(0,(wid-1))
         start_y = 0
         matrix[start_y][start_x] = 1
-        # print(matrix)
+        print(matrix)
         move = True
         while (move ==  True):
-            next_pos = move_action(start_x,start_y)
-            
+            next_pos = move_action(start_y,start_x)
+            matrix[start_y][start_x] = 0
             if(next_pos[0] < 0 or next_pos[1] < 0):
-                print("Aqui", next_pos)
+                # print("Aqui", next_pos)
                 destroy += 1
                 move = False
 
-            if(next_pos[0] >= wid or next_pos[1] >= hig):
-                print("ou aqui", next_pos)
+            elif(next_pos[0] >= hig or next_pos[1] >= wid):
+                # print("ou aqui", next_pos)
                 destroy += 1
                 move = False
+            elif(check_neighbors(next_pos[0],next_pos[1],matrix) == True):
+                move = False
+                matrix[next_pos[0]][next_pos[1]] = 1
+                start_y = next_pos[0]
+                start_x = next_pos[1]
+                # print("Turning seed: ", next_pos)
             else:
-                matrix[start_y][start_x] = 0
-                matrix[next_pos[1]][next_pos[0]] = 1
-                start_x = next_pos[0]
-                start_y = next_pos[1]
-                if(check_neighbors(next_pos[1],next_pos[0],matrix) == True):
-                    move = False
-            # print(matrix)
+                matrix[next_pos[0]][next_pos[1]] = 1
+                start_y = next_pos[0]
+                start_x = next_pos[1]
+                # print("seguiu para: ", next_pos)
+                
+            print(matrix)
 
             # move = False
-    print(matrix)
-    print(seeds_number, seeds_distance)
+    # print(matrix)
+    # print(seeds_number, seeds_distance)
     print( "Drops destroyeds: ", destroy)
-ald(1000,1000,1,10000)
+ald(30,24,1,1000)
